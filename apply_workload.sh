248bad8ceebc6b73cd75b2be49eb95ebbbe93ea0
Benoit Moussaud <benoit@moussaud.org>
2023-03-22 15:35:42 +0100

    update configuration

--- apply_workload.sh
+++ apply_workload.sh
@@ -1,4 +1,4 @@
 NS=micropets-dev
 kubectl apply -f config/elephants-configuration.yaml --namespace $NS
-kubectl apply -f config/ops/mongodb-operator/00-mongodb-operator.yaml -f config/ops/k8s 
-tanzu apps workload apply -f config/workload.yaml  --live-update --local-path . --source-image akseutap4registry.azurecr.io/elephants --namespace $NS --yes  --update-strategy merge
+kubectl apply -f config/ops/k8s 
+tanzu apps workload apply -f config/workload.yaml  --live-update --local-path . --source-image akseutap5registry.azurecr.io/elephants --namespace $NS --yes  --update-strategy merge
